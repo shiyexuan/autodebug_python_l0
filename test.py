6b61f9bb87f98d41d4937ca9c2f2be76933e9749
shiyexuan <shiyexuan@bytedance.com>
2024-07-03 02:44:11 +0000

    stack overflow

--- test.py
+++ test.py
@@ -1,21 +1,11 @@
-# L0 底线用例：变量使用错误
+# L0 底线用例：堆栈溢出
 
-def knapsack(weights, values, capacity):
-    n = len(weights)
-    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n + 1)
 
-    for i in range(1, n + 1):
-        for j in range(1, capacity + 1):
-            if weights[i - 1] <= j:
-                dp[i][j] = max(values[i - 1] + dp[i - 1][j - weights[i - 1]], dp[i - 1][j])
-            else:
-                dp[i][j] = dp[i - 1][j]
-
-    return dp[n][capacity][0]
-
-weights = [2, 3, 4, 5]
-values = [3, 4, 5, 6]
-capacity = 8
-
-max_value = knapsack(weights, values, capacity)
-print("背包能装的最大价值为:", max_value)
+num = 5
+result = factorial(num)
+print(f"{num}的阶乘为：{result}")
