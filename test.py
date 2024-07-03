6f7351ed3634c3d40841e45b0199b54415253a37
shiyexuan <shiyexuan@bytedance.com>
2024-07-03 02:54:16 +0000

    spell error

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
-# L0 底线用例：数组越界
+# L0 底线用例：拼写错误
 
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n - 1):
-        for j in range(n - i):
+        for j in range(n - i - 1):
             if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                arr[j], arr[j + 1] = arrj[1], arr[j]
 
-# 示例用法
 nums = [5, 3, 8, 2, 1]
 bubble_sort(nums)
 print("排序后的数组:", nums)
