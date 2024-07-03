77f2a96a1a37b01b16a58dff142172a90b35d5c6
shiyexuan <shiyexuan@bytedance.com>
2024-07-03 03:12:34 +0000

    import error

---
+++ sort.py
@@ -0,0 +1,15 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
--- test.py
+++ test.py
@@ -1,12 +1,7 @@
-# L0 底线用例：拼写错误
+# L0 底线用例：引用错误
 
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n - 1):
-        for j in range(n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arrj[1], arr[j]
+from sort import bub_sort
 
 nums = [5, 3, 8, 2, 1]
-bubble_sort(nums)
+bub_sort(nums)
 print("排序后的数组:", nums)
