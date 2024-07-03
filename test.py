c593d91fabd9fecd990ed9e9b245294c80a6b2d4
shiyexuan <shiyexuan@bytedance.com>
2024-07-03 06:17:43 +0000

    class missing

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-# L0 底线用例：类方法缺失
+# L0 底线用例：简单的类缺失
 
-from animals import Zoo, Dog, Cat
+from animals import Zoo, Dog, Cat, Bird
 
-zoo = Zoo([Dog("旺财"), Cat("咪咪")])
+zoo = Zoo([Dog("旺财"), Cat("咪咪")], Bird("小鸟"))
 
 print("动物园里有：", len(zoo), " 只动物")
 zoo.make_sound()
