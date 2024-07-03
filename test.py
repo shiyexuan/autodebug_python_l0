cf89840cdc6b4dae6f2425f197e5b6e3c3032945
shiyexuan <shiyexuan@bytedance.com>
2024-07-03 03:58:37 +0000

    implemetation error

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
-# L0 底线用例：传参错误
+# L0 底线用例：函数实现错误
 
-from sort import bubble_sort
+from sort import bubble_sort, insertion_sort
 
-nums = [5, 3, 8, 2, 1]
-bubble_sort(nums, dec = True)
-print("排序后的数组:", nums)
+nums1 = [5, 3, 8, 2, 1]
+nums2 = [5, 8, 3, 2, 1]
+bubble_sort(nums1, dec = True)
+insertion_sort(nums2, dec = True)
+assert(nums1 == nums2)
+
+print("排序后的数组:", nums1)
